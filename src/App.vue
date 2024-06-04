bd8d36d31ed86ece013147345f4f02ebf721cc27
조유림 <joyurim1989@10x10.co.kr>
2024-06-04 13:36:06 +0900

    vue, scss 설지

---
+++ src/App.vue
@@ -0,0 +1,38 @@
+<template>
+  <div :class="{'lg': isMobile}">
+    <ul>
+      <li>메뉴명</li>
+    </ul>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'App',
+  components: {
+  },
+  data() {
+    return {
+      isMobile: false
+    };
+  },
+  methods: {
+    checkIfMobile() {
+      this.isMobile = window.innerWidth >= 750;
+    }
+  },
+  mounted() {
+    this.checkIfMobile();
+    window.addEventListener('resize', this.checkIfMobile);
+  },
+  beforeUnmount() {
+    window.removeEventListener('resize', this.checkIfMobile);
+  },
+}
+</script>
+
+<style lang="scss">
+@import 'normalize.css';
+@import '@/styles/common.scss';
+</style>
