0601d25bf04ba3be850e8b36d2429aa29e2d7d73
조유림 <joyurim1989@10x10.co.kr>
2024-06-25 13:57:01 +0900

    상품 리스트 공통 컴포넌트 추가

---
+++ src/components/10x10/common/ProductItem.vue
@@ -0,0 +1,97 @@
+<template>
+  <li class="item">
+    <figure class="item__thumbnail">
+      <img src="" alt="">
+    </figure>
+    <p class="item__brand">brand</p>
+    <p class="item__name">Puffy pattern 11인치 아이패드 태블릿 파우치 - 라일락</p>
+    <p class="item__orgPrice">11,000</p>
+    <p class="item__priceBox">
+      <span class="percent">10%</span>
+      <span class="price">10,000</span>
+    </p>
+    <div class="review">
+      <div class="review__eval">
+        <span class="review__eval--rating" style="width: 20%;" />
+      </div>
+      <span class="review__count">
+        (204)
+      </span>
+    </div>
+  </li>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+    };
+  }
+}
+</script>
+<style scoped lang="scss">
+.item {
+  width: pxToVw(150);
+  &__thumbnail {
+    @include thumbnail(150, 150);
+    margin-bottom: pxToVw(8);
+  }
+  &__brand {
+    @include font(14,20,$weight-700);
+  }
+  &__name {
+    overflow: hidden;
+    text-overflow: ellipsis;
+    display: -webkit-box;
+    -webkit-line-clamp: 2;
+    -webkit-box-orient: vertical;
+    margin-bottom: pxToVw(8);
+    @include font(14,20,$weight-500);
+    letter-spacing: - pxToVw(0.5);
+  }
+  &__orgPrice {
+    color: $grey50;
+    @include font(12,18,$weight-500);
+    letter-spacing: - pxToVw(0.09);
+    text-decoration-line: line-through;
+  }
+  &__priceBox {
+    .percent {
+      margin-right: pxToVw(4);
+      color: $ten;
+      @include font(15,20,$weight-700);
+      letter-spacing: - pxToVw(0.09);
+    }
+    .price {
+      @include font(15,20,$weight-700);
+      letter-spacing: - pxToVw(0.09);
+    }
+  }
+  .review {
+    display: flex;
+    align-items: center;
+    margin-top: pxToVw(8);
+    &__eval {
+      display: inline-block;
+      width: pxToVw(60);
+      height: pxToVw(12);
+      background-image: url("@/assets/tenbyten/common/star-line.svg");
+      background-repeat: repeat-x;
+      background-size: pxToVw(12) pxToVw(12);
+      overflow: hidden;
+      &--rating {
+        display: inline-block;
+        height: pxToVw(12);
+        background-image: url("@/assets/tenbyten/common/star-full.svg");
+        background-repeat: repeat-x;
+        background-size: pxToVw(12) pxToVw(12);
+      }
+    }
+    &__count {
+      margin-left: pxToVw(4);
+      color: $grey60;
+      @include font(11,16,$weight-400);
+    }
+  }
+}
+</style>
--- src/components/10x10/main/TenByTenMain.vue
+++ src/components/10x10/main/TenByTenMain.vue
@@ -23,6 +23,27 @@
         <img :src="main.img" alt="main.title">
       </swiper-slide>
     </swiper>
+    <ul class="gnb">
+      <li v-for="(gnb, index) in gnbList" :key="`gnblist-`+index">
+        <a :href="gnb.href" target="_blank">
+          <div class="gnb__img">
+            <img :src="gnb.img" :alt="gnb.name">
+          </div>
+          <p class="gnb__name">{{gnb.name}}</p>
+        </a>
+      </li>
+    </ul>
+    <section class="recommend">
+      <h2 class="recommend__title">
+        고객님, 이 상품은 어떠세요?
+        <span class="recommend__title--sponsored">
+          Sponsored
+        </span>
+      </h2>
+      <ul class="recommend__container">
+        <ProductItem />
+      </ul>
+    </section>
   </main>
 </template>
 
@@ -30,9 +51,10 @@
 import {defineComponent} from 'vue'
 import {Swiper, SwiperSlide} from "swiper/vue";
 import {Autoplay, Pagination} from "swiper/modules";
+import ProductItem from "@/components/10x10/common/ProductItem.vue";
 export default defineComponent({
   name: "TenByTenMain",
-  components: {Swiper, SwiperSlide},
+  components: {ProductItem, Swiper, SwiperSlide},
   data() {
     return {
       modules: [Autoplay, Pagination],
@@ -54,6 +76,58 @@ export default defineComponent({
           title: '오늘부터 식집사<br/>반려식물을 입양하세요.',
           subText:'~58% 할인',
           subTextBgColor: '#0DBA14'
+        },
+      ],
+      gnbList: [
+        {
+          href: 'https://m.10x10.co.kr/event/eventmain.asp?eventid=128019',
+          img: require('@/assets/tenbyten/main/gnb01.png'),
+          name: '선착순 특가'
+        },
+        {
+          href: 'https://m.10x10.co.kr/event/eventmain.asp?eventid=127725',
+          img: require('@/assets/tenbyten/main/gnb02.png'),
+          name: '문구를 말하다'
+        },
+        {
+          href: 'https://m.10x10.co.kr/event/eventmain.asp?eventid=127962',
+          img: require('@/assets/tenbyten/main/gnb03.png'),
+          name: '그녀들의 식탁'
+        },
+        {
+          href: 'https://m.10x10.co.kr/event/eventmain.asp?eventid=127937',
+          img: require('@/assets/tenbyten/main/gnb04.png'),
+          name: '한번만 써봐요'
+        },
+        {
+          href: 'https://m.10x10.co.kr/event/eventmain.asp?eventid=127803',
+          img: require('@/assets/tenbyten/main/gnb05.png'),
+          name: '텀꾸 추천템'
+        },
+        {
+          href: '',
+          img: require('@/assets/tenbyten/main/gnb06.png'),
+          name: '선물의 참견'
+        },
+        {
+          href: '',
+          img: require('@/assets/tenbyten/main/gnb07.png'),
+          name: '선물의 진심'
+        },
+        {
+          href: '',
+          img: require('@/assets/tenbyten/main/gnb08.png'),
+          name: '이벤트'
+        },
+        {
+          href: '',
+          img: require('@/assets/tenbyten/main/gnb09.png'),
+          name: '텐텐배송'
+        },
+        {
+          href: '',
+          img: require('@/assets/tenbyten/main/gnb10.png'),
+          name: '베스트'
         },
       ]
     }
